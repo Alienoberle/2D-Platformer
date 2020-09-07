a07a070f8e855f0b10bfd775784913e0a80067dd
Christian Oberle <alienoberle@hotmail.com>
2020-09-07 22:48:46 +0200

    CHANGED: Leveloading and Main Menu

---
+++ Assets/Scripts/Menu/LevelLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public Animator defaultTransitionAnimation;
+
+    [SerializeField]
+    private string nameOfSceneToLoad;
+   
+    [SerializeField]
+    private float transitionTime = 1.0f;
+
+
+    public void LoadLevel(string nameOfSceneToLoad, Animator transitionAnimation)
+    {
+        StartCoroutine(LevelTransition(nameOfSceneToLoad, transitionAnimation));
+    }
+
+    public void LoadLevel(string nameOfSceneToLoad)
+    {
+        StartCoroutine(LevelTransition(nameOfSceneToLoad, defaultTransitionAnimation));
+    }
+
+    IEnumerator LevelTransition(string nameOfSceneToLoad, Animator transitionAnimation)
+    {
+        transitionAnimation.SetTrigger("StartTransitionIn");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(nameOfSceneToLoad);
+    }
+
+}
---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public LevelLoader levelLoader;
+
+    private void Awake()
+    {
+        levelLoader = FindObjectOfType<LevelLoader>();
+    }
+
+    public void Play()
+    {
+        levelLoader.LoadLevel("Testmap");
+    }
+
+    public void Options()
+    {
+        Debug.Log("Options Menu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+
+
+}
